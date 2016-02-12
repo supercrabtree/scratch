c4df6dc6203e1e8a80639ea87162eec597f7d976
George Crabtree <supercrabtree@googlemail.com>
2016-02-12 10:44:35 +0100

    Extract user/repo#branch from $2

--- scratch.sh
+++ scratch.sh
@@ -33,71 +33,54 @@ scratch() {
     local minor_git_version=$(git --version | sed 's/\(git version \)\([0-9]*\.[0-9]*\)\(.*\)/\2/')
     echo "$1 < ${minor_git_version}" | bc
   }
-  __scratch_create_remote_addresses() {
-    # the following styles will all result in the two remote addresses
-    # http://github.com/supercrabtree/kerpow.git
-    # git@github.com:supercrabtree/kerpow.git
+
+  __extract_repo_info() {
+    local repo
+
+    # will extract "username/repo#commit-ish" from the following:
     #
-    # scratch install kerpow
-    # scratch install supercrabtree/kerpow
-    # scratch install http://github.com/supercrabtree/kerpow
-    # scratch install git@github.com:supercrabtree/kerpow.git
-    # scratch install https://github.com/supercrabtree/kerpow.git
-    # scratch install http://github.com/supercrabtree/kerpow.git
-    local ssh_address http_address
+    # supercrabtree/kerpow
+    # supercrabtree/kerpow#6c7efc4
+    # git@github.com:supercrabtree/kerpow.git
+    # https://github.com/supercrabtree/kerpow
+    # https://github.com/supercrabtree/kerpow.git
 
-    # did they pass in an http or https git repo address
-    local repo=$(printf %s $to_install | awk '/^http(s)?\:\/\/.*\.git$/')
-    if [ -n "$repo" ];then
-      printf '%s %s' "" $repo
+    # did they pass in a username/repo
+    # i.e. supercrabtree/beepboop
+    repo=$(printf %s $1 | sed -n 's/\(^[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9]\/[a-zA-Z0-9\-]*$\)/\1/p')
+    if [ ! -z $repo ];then
+      printf '%s#' $repo
       return 0
     fi
 
-    # did they pass in an http or https git repo address
-    local repo=$(printf %s $to_install | awk '/^git@.*\.git$/')
-    if [ -n "$repo" ];then
-      printf '%s %s' $repo ""
+    # did they pass in a username/repo#commit-ish
+    # i.e. supercrabtree/beepboop#6c778ccd
+    repo=$(printf %s $1 | sed -n 's/\(^[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9]\/[a-zA-Z0-9\-]*#.*$\)/\1/p')
+    if [ ! -z $repo ];then
+      printf '%s' $repo
       return 0
     fi
 
-    # scratch install kerpow
-    local user_github_repo=$(printf %s $1 | awk '/^[0-9a-z]*$/')
-    if [ -n "$user_github_repo" ];then
-      # TODO: workout how to get their github username
-      git_hub_user_name="<github-username>"
-      ssh_address="git@github.com:$git_hub_user_name/$user_github_repo.git"
-      http_address="https://github.com/$git_hub_user_name/$user_github_repo.git"
-      printf '%s %s' $ssh_address $http_address
+    # did they pass in an https git repo address
+    # i.e. https://github.com/supercrabtree/beepboop.git                         = supercrabtree/beepboop
+    # or   https://github.com/supercrabtree/beepboop                             = supercrabtree/beepboop
+    # or   https://github.com/supercrabtree/beepboop/so/deep/nested/whatever     = supercrabtree/beepboop
+    repo=$(printf %s $1 | sed -n 's/https\:\/\/github.com\/\([^\/]*\)\/\([^\.\/]*\)\(.*\)/\1\/\2/p')
+    if [ ! -z $repo ];then
+      printf '%s#' $repo
       return 0
     fi
 
-    # scratch install supercrabtree/kerpow
-    local github_repo=$(printf %s $1 | awk '/^[0-9a-z]*\/[0-9a-z]*$/')
-    if [ -n "$github_repo" ];then
-      ssh_address="git@github.com:$github_repo.git"
-      http_address="https://github.com/$github_repo.git"
-      printf '%s %s' $ssh_address $http_address
+    # did they pass in an ssh git repo address
+    # i.e. git@github.com:supercrabtree/beepboop.git = supercrabtree/beepboop
+    repo=$(printf %s $1 | sed -n 's/git@github.com:\([^\/]*\)\/\([^\/]*\)\.git/\1\/\2/p')
+    if [ ! -z $repo ];then
+      printf '%s#' $repo
       return 0
     fi
 
-    # scratch install http://github.com/supercrabtree/kerpow
-    local repo=$(printf %s $1 | awk '/^http(s)?\:\/\/[0-9a-z]/')
-    if [ -n "$repo" ];then
-      # remove any trailing slashes
-      repo=$(printf %s $1 | sed 's/\/*$//')
-      # strip off http:// or https:// or http://www. or http://www.
-      local stripped_repo=$(printf %s $repo | sed 's/https*\:\/\/w*\.*//')
-      ssh_address="git@$(printf %s $stripped_repo | sed 's/\//:/').git"
-      http_address="$repo.git"
-      printf '%s %s' $ssh_address $http_address
-      return 0
-    fi
+
   }
-
-
-  # define varibles to be used in the script
-  # ----------------------------------------------------------------------------
-  local name folder
 
 
   # run script
@@ -125,6 +108,7 @@ scratch() {
     cd $name
   fi
 
+
   # if `scratch install`
   if [ "$1" = "install" ]; then
     # Check to make sure the next parameter has also been supplied
@@ -140,10 +124,19 @@ scratch() {
       printf '%s\n' "Stop the press!! it's a folder. Sorry not supported yet."
       return 0
     fi
+
+    local repo_info=$(__extract_repo_info $to_install)
+    local repo_name=$(printf %s $repo_info | sed 's/#.*//')
+    local repo_branch=$(printf %s $repo_info | sed 's/.*#//')
+
+    printf 'all:    %s\n' $repo_info
+    printf 'name:   %s\n' $repo_name
+    printf 'branch: %s\n' $repo_branch
   fi
 
   unset -f __rand_char
   unset -f __rand_word
   unset -f __rand_number
+  unset -f __extract_repo_info
 }
 
